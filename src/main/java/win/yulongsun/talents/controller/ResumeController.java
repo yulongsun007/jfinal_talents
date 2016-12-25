a64bded44ea2331ed3f9eeffa1655f51a88fa6e2
yulongsun007 <a87533887>
2016-12-26 04:52:42 +0800

    1.修复简历头像修改

--- src/main/java/win/yulongsun/talents/controller/ResumeController.java
+++ src/main/java/win/yulongsun/talents/controller/ResumeController.java
@@ -70,11 +70,6 @@ public class ResumeController extends BaseController {
     }
 
     public void update() {
-        String     has_img = getPara("has_img");
-        UploadFile file    = null;
-        if ("1".equals(has_img)) {
-            file = getFile("resume_img");
-        }
         Integer resume_id          = getParaToInt("resume_id");
         String  resume_name        = getPara("resume_name");
         String  resume_desc        = getPara("resume_desc");
@@ -93,9 +88,52 @@ public class ResumeController extends BaseController {
         }
         Resume resume = new Resume();
         resume.setResumeId(resume_id);
-        if (file != null) {
-            resume.setResumeImg(file.getFileName());
+        resume.setResumeName(resume_name);
+        resume.setResumeDesc(resume_desc);
+        resume.setResumeGender(resume_gender);
+        resume.setResumeAcademy(resume_academy);
+        resume.setResumeIsStudy(resume_is_study);
+        resume.setResumeMajor(resume_major);
+        resume.setResumeGraduateAt(resume_graduate_at);
+        resume.setResumeMobile(resume_mobile);
+        resume.setResumeEmail(resume_email);
+        resume.setCreateBy(create_by);
+        resume.setUpdateAt(new Date());
+        boolean save = resume.update();
+        if (save) {
+            List<Resume> resumes = Resume.dao.find("select * from t_resume where create_by = ? order by resume_id desc", create_by);
+            for (Resume r : resumes) {
+                r.setResumeImg(IPUtils.getUploadPath() + r.getResumeImg());
+                List<ResumeExper> experList = ResumeExper.dao.findByResumeId(r.getResumeId());
+                r.put("experList", experList);
+            }
+            renderSuccess(resumes);
+        } else {
+            renderError(Response.MSG.ADD_ERROR);
         }
+    }
+
+    public void updateImg() {
+        UploadFile file               = getFile("resume_img");
+        Integer    resume_id          = getParaToInt("resume_id");
+        String     resume_name        = getPara("resume_name");
+        String     resume_desc        = getPara("resume_desc");
+        String     resume_gender      = getPara("resume_gender");
+        String     resume_academy     = getPara("resume_academy");
+        String     resume_is_study    = getPara("resume_is_study");
+        String     resume_major       = getPara("resume_major");
+        String     resume_graduate_at = getPara("resume_graduate_at");
+        String     resume_mobile      = getPara("resume_mobile");
+        String     resume_email       = getPara("resume_email");
+        Integer    create_by          = getParaToInt("create_by");
+        boolean isNull = ValidateUtils.validatePara(resume_id, resume_name, resume_desc, resume_gender, resume_academy, resume_is_study, resume_major,
+                resume_graduate_at, resume_mobile, resume_email, create_by);
+        if (isNull) {
+            renderError(Response.MSG.REQ_IS_NULL);
+        }
+        Resume resume = new Resume();
+        resume.setResumeId(resume_id);
+        resume.setResumeImg(file.getFileName());
         resume.setResumeName(resume_name);
         resume.setResumeDesc(resume_desc);
         resume.setResumeGender(resume_gender);
